1961fb7b83625fa90b5365e568e30422368e5d11
guydudai <guydudai@mail.tau.ac.il>
2025-12-11 21:33:35 +0200

    Remove secrets from config.py - use env vars

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
+import os
 
-#Google sheets configuration
-APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxarYzowDTUn40WNahypMaTOpjCWz4drmdV2wCvkyYlSNUZgur4pBCoiK0dSU4jTZip/exec"
+# Google sheets configuration
+APPS_SCRIPT_URL = os.environ.get("APPS_SCRIPT_URL", "")
+
 # Firebase Configuration
-KEY_PATH = 'beit-leah-soldiers-firebase-adminsdk-fbsvc-6f3f1f15fb.json'
+KEY_PATH = os.environ.get("KEY_PATH", "")
 
 # WhatsApp API Configuration
-WHATSAPP_TOKEN = "EAApZAR0f6FtABQEszOH7JhqoLZAsnpcZAhIZBBZAoNQHRXN2tkJ2eqlt1ZBftdjlIr1ZBYxfBCG3kznGsMMRbQTkU3MKE7JpuPGlZBaJtRP8PaOBQlGyt8qf0EyLUX4CbmNUu9iRWTvDmnfv9BK06nUo6LmVgJtoivuoYKiKE7dl8S6WI6EKlVCXGvRLu2a270eGLAZDZD"
-PHONE_NUMBER_ID = "948193088373337"  
-BOT_PHONE_NUMBER = "+1 555 608 3075"
-VERIFY_TOKEN = "YOUR_VERIFY_TOKEN_HERE"
+WHATSAPP_TOKEN = os.environ.get("WHATSAPP_TOKEN", "")
+PHONE_NUMBER_ID = os.environ.get("PHONE_NUMBER_ID", "")
+BOT_PHONE_NUMBER = os.environ.get("BOT_PHONE_NUMBER", "")
+VERIFY_TOKEN = os.environ.get("VERIFY_TOKEN", "")
 
 # Beit Leah URL
-BEIT_LEAH_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfufSxNBjrfO0FPPl7r2g9SXhPhB1KX65BJFdmm7eBCev8wfQ/viewform"
+BEIT_LEAH_URL = os.environ.get("BEIT_LEAH_URL", "")
